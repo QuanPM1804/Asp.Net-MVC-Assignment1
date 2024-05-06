305d220c5fa03684bd52dc71e4e2a3b7079b067b
QuanPM <95131155+QuanPM1804@users.noreply.github.com>
2024-05-06 21:43:46 +0700

    Add files via upload

---
+++ Controllers/RookiesController.cs
@@ -0,0 +1,91 @@
+﻿using OfficeOpenXml;
+using Asp_MVC1.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Asp_MVC1.Controllers
+{
+    public class RookiesController : Controller
+    {
+        private static List<Person> persons = new List<Person>
+        {
+            new Person { FirstName = "Van A", LastName = "Nguyen", Gender = "Male", DateOfBirth = new DateTime(2000, 3, 15), PhoneNumber = "1234567890", BirthPlace = "Ha Noi", IsGraduated = true },
+            new Person { FirstName = "Van B", LastName = "Nguyen", Gender = "Male", DateOfBirth = new DateTime(1999, 6, 20), PhoneNumber = "9876543210", BirthPlace = "Sai Gon", IsGraduated = false },
+            new Person { FirstName = "Thi C", LastName = "Nguyen", Gender = "Female", DateOfBirth = new DateTime(2001, 6, 20), PhoneNumber = "9876543210", BirthPlace = "Sai Gon", IsGraduated = false },
+        };
+        public IActionResult Index()
+        {
+            return View(persons);
+        }
+        public IActionResult MaleMembers()
+        {
+            var maleMembers = persons.Where(p => p.Gender == "Male").ToList();
+            return View("Result", maleMembers);
+        }
+
+        public IActionResult OldestMember()
+        {
+            var oldestMember = persons.OrderBy(p => p.DateOfBirth).FirstOrDefault();
+            return View("Result", oldestMember);
+        }
+
+        public IActionResult FullNames()
+        {
+            var fullNames = persons.Select(p => $"{p.LastName} {p.FirstName}").ToList();
+            return View("Result", fullNames);
+        }
+
+        public IActionResult FilterByBirthYear(int year)
+        {
+            var filteredMembers = persons.Where(p => p.DateOfBirth.Year == year).ToList();
+            return View("Result", filteredMembers);
+        }
+        public IActionResult ExportToExcel()
+        {
+            var stream = new MemoryStream();
+            ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+            using (var package = new ExcelPackage(stream))
+            {
+                var worksheet = package.Workbook.Worksheets.Add("Persons");
+                worksheet.Cells["A1"].LoadFromCollection(persons.Select(p => new
+                {
+                    p.FirstName,
+                    p.LastName,
+                    p.Gender,
+                    DateOfBirth = p.DateOfBirth.ToString("yyyy-MM-dd"),
+                    p.PhoneNumber,
+                    p.BirthPlace,
+                    p.IsGraduated
+                }), true);
+
+                package.Save();
+            }
+
+            stream.Position = 0;
+            var fileName = $"Persons_{DateTime.Now.ToString("yyyyMMdd_HHmmss")}.xlsx";
+            return File(stream, "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", fileName);
+        }
+        [HttpPost]
+        public IActionResult HandleOption(int option)
+        {
+            switch (option)
+            {
+                case 1:
+                    return RedirectToAction("MaleMembers");
+                case 2:
+                    return RedirectToAction("OldestMember");
+                case 3:
+                    return RedirectToAction("FullNames");
+                case 4:
+                    return RedirectToAction("FilterByBirthYear", new { year = 2000 });
+                case 5:
+                    return RedirectToAction("FilterByBirthYear", new { year = 2001 });
+                case 6:
+                    return RedirectToAction("FilterByBirthYear", new { year = 1999 });
+                case 7:
+                    return RedirectToAction("ExportToExcel");
+                default:
+                    return RedirectToAction("Index");
+            }
+        }
+    }
+}
---
+++ Models/Person.cs
@@ -0,0 +1,16 @@
+﻿using Asp_MVC1.Models;
+
+namespace Asp_MVC1.Models
+{
+    public class Person
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Gender { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string PhoneNumber { get; set; }
+        public string BirthPlace { get; set; }
+        public bool IsGraduated { get; set; }
+        public string FullName => LastName + ' ' + FirstName;
+    }
+}
